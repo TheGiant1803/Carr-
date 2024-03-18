26baffed03ec145768fe50fa7a7b7e07c08dc1f6
Antoine <agaujour@gmail.com>
2024-03-18 14:27:00 +0100

    v3 avec un max de nouveauté et de canard

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-int carre(int i){
+﻿/* Sous-programme : calcule coin coin le petit canard 
+    Paramètre d'entrée:
+    -i : entier dont on souhaite le carré (entrée)
+    valeur de retour : le carré de i */int carre(int i){
     return i*i;
 }
 int val=Convert.ToInt32(Console.ReadLine());
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Carr-
 Uwu
+Ce programme
+Draco Dormiens Nunquam Titillandus
